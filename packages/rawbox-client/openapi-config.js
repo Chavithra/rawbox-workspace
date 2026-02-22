793457f15e6180978766e59d63ebee75d75ce5d1
Chavithra <chavithra@gmail.com>
2026-02-22 11:23:29 +0100

    rawbox-client: stack configuration

--- packages/rawbox-client/openapi-config.js
+++ packages/rawbox-client/openapi-config.js
@@ -1,6 +1,5 @@
-import type { ConfigFile } from "@rtk-query/codegen-openapi";
-
-const config: ConfigFile = {
+/** @type {import('@rtk-query/codegen-openapi').ConfigFile} */
+const config = {
   schemaFile: "http://127.0.0.1:3000/api/v1/swagger/json",
   apiFile: "./src/redux/rawbox-api-empty.ts",
   apiImport: "rawboxApiEmpty",
